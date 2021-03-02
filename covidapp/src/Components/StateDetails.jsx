e6301ebc8f9f7b6f117b4d3f83463c62224f6191
kel <kelpius21@gmail.com>
2021-03-02 13:51:00 -0500

    Updated StateDetails.jsx

--- covidapp/src/Components/StateDetails.jsx
+++ covidapp/src/Components/StateDetails.jsx
@@ -3,32 +3,56 @@ import React, { useState, useEffect } from 'react';
 function StateDetails({match}) {
 
     const [stateDetail, setStateDetail] = useState({});
-    const [businessName, setBusinessName] = useState({});
+    const [locations, setLocations] = useState([]);
+    // const [businessName, setBusinessName] = useState({});
+
+    const getLocations = (response) => {
+        let locationArray = []
+        response.forEach((location) => {
+            fetch(location)
+            .then((res) => res.json())
+            .then(res => {
+                setLocations(locations.concat(res))
+                // let locationArray = [locations];
+                locationArray.push(res);
+                // setLocations(locationArray)
+                console.log(locationArray)
+                setLocations(locationArray)
+            })
+        })
+    }
 
     useEffect(() => {
         fetch(`http://localhost:8000/state/${match.params.id}`)
             .then((res) => res.json())
             .then(res => {
-                // console.log(res)
+                console.log(res)
                 setStateDetail(res)
-            })
-            .catch(console.error);
-    }, []);
-
-    useEffect(() => {
-        fetch(`http://localhost:8000/locations/${match.params.id}`)
-            .then((res1) => res1.json())
-            .then(res1 => {
-                console.log(res1)
-                setBusinessName(res1)
+                getLocations(res.locations)
             })
             .catch(console.error);
     }, []);
 
 
-    console.log(businessName.business_name)
-    console.log(businessName.hours)
-    console.log(businessName.address)
+
+    // useEffect(() => {
+    //     fetch(`http://localhost:8000/locations/${match.params.id}`)
+    //         .then((res1) => res1.json())
+    //         .then(res1 => {
+    //             console.log(res1)
+    //             setBusinessName(res1)
+    //         })
+    //         .catch(console.error);
+    // }, []);
+
+
+    // console.log(businessName)
+    // console.log(businessName.hours)
+    // console.log(businessName.address)
+    console.log(locations)
+
+    // console.log(setStateDetail)
+
 
     return (
         <div>
@@ -37,9 +61,16 @@ function StateDetails({match}) {
             <h3>Average Daily Cases: {stateDetail.average_daily_cases}/day</h3>
             <h3>Get tested / vaccinated at the following locations:</h3>
 
-            {stateDetail.locations && stateDetail.locations.map((location) => (
-                <h3>{`business name:${location.address}`}</h3>
+            {stateDetail.locations && locations.map((location) => (
+                <h3>{`business name:${location.business_name}`}</h3>
             ))}
+
+            {/* {businessName && businessName.business_name.map((businessName) => (
+                <h3>{`business name:${businessName.address}`}</h3>
+            ))} */}
+
+            {/* <h3>{businessName.business_name}</h3>
+            <h3>{businessName.address}</h3> */}
 
         </div>
     );
