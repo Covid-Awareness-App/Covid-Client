7b793819c726a2eb95664b29ab0b1a9a329c8bd4
kel <kelpius21@gmail.com>
2021-03-02 19:38:36 -0500

    Updated Locations Details page.

--- covidapp/src/Components/Navbar.jsx
+++ covidapp/src/Components/Navbar.jsx
@@ -1,6 +1,8 @@
 import React, {  useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
+// import { Nav, NavItem, NavLink } from 'reactstrap';
+
 const Navbar = () => {
 
     const [newState, setNewState] = useState([]);
